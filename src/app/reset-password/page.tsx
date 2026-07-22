0fe15a29863e93018490fe018fb6807877bc852a
DevCaioBO <caiobuenooliveira1210@gmail.com>
2026-07-21 22:23:41 -0300

    fix: modify name of page reset-password target not conflict with component call

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,8 @@
+import ResetPasswordForm from "@/src/features/auth/components/reset-password-form";
 import { Suspense } from "react";
 
 
-export default function ResetPasswordForm() {
+export default function ResetPasswordFormPage() {
    return(
     <Suspense fallback={<div className="h-dvh flex items-center justify-center">Carregando...</div>}>
 
